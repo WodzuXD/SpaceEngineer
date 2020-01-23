cf0287ab2273678ebd486133d071dd958ebbc7fb
Woszu <kruser757@gmail.com>
2020-01-23 10:50:39 +0100

    Optimalize

--- Space Ships Mecanic/Assets/_Scripts/Zlecenia.cs
+++ Space Ships Mecanic/Assets/_Scripts/Zlecenia.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Zlecenia : MonoBehaviour
@@ -10,6 +11,13 @@ public class Zlecenia : MonoBehaviour
 
     public GameObject canvas;
 
+    public bool scena = false;
+
+    Zlecenia zla;
+    public List<Zlecenia> zl = new List<Zlecenia>();
+    public List<dousunecia> pod = new List<dousunecia>();
+
+    bool dodawanie = false;
 
     private void Start()
     {
@@ -19,6 +27,26 @@ public class Zlecenia : MonoBehaviour
 
     private void Update()
     {
+        zla = FindObjectOfType<Zlecenia>();
+        if (SceneManager.sceneCount != 1)
+            scena = true;
+        if (SceneManager.sceneCount == 1 && scena)
+        {
+            for (int i = 0; i < zl.Count; i++)
+            {
+                Destroy(zl[i].gameObject);
+                zl.Remove(zl[i]);
+            }
+            for (int i = 0; i < pod.Count; i++)
+            {
+                Destroy(pod[i].gameObject);
+                pod.Remove(pod[i]);
+            }
+        }
+        if (SceneManager.sceneCount == 1 && !scena && !dodawanie)
+            if (zla.scena)
+                 zla.zl.Add(gameObject.transform.GetComponent<Zlecenia>()); dodawanie = true;
+        
         if (przyjete && zlecenia != 0)
         {
             canvas.active = false;
